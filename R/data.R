328f0e6730f09f3438580bede020369b4037c2e6
rmsheridan@cassini <ryan.sheridan@ucdenver.edu>
2020-09-25 23:09:52 -0600

    documentation for data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' AVID-seq data for mouse splenocytes
+#'
+#' A Seurat object containing AVID-seq data for mouse splenocytes
+#'
+#' @format A Seurat object with 12334 features, 7345 cells, and 3 assays
+"so"
+
+#' AVID-seq data for tests
+#'
+#' A tiny Seurat object containing AVID-seq data used for tests
+#'
+#' @format A Seurat object with 12334 features, 100 cells, and 3 assays
+"test_so"
