62329fb647b548c942dbf6b08d183677b31fd5f2
rmsheridan <ryan.sheridan@ucdenver.edu>
2020-10-14 14:25:00 -0600

    tests

--- R/data.R
+++ R/data.R
@@ -9,11 +9,11 @@
 #'
 #' A tiny Seurat object for tests
 #'
-#' @format A Seurat object with 100 features, 100 cells, and 3 assays
+#' @format A Seurat object with 100 features, 100 cells, and 1 assay
 "test_so"
 
 #' Seurat object with V(D)J data for tests
 #'
 #' A tiny Seurat object with V(D)J data for tests
-#' @format A Seurat object with 100 features, 100 cells, and 3 assays
+#' @format A Seurat object with 100 features, 100 cells, and 1 assay
 "test_vdj"
