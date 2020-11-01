64599f6594c071d3053dc2a5f6d6536f802d6bef
rmsheridan@cassini <ryan.sheridan@ucdenver.edu>
2020-11-01 09:55:05 -0700

    fix import_vdj tests

--- tests/testthat/test_import_vdj.R
+++ tests/testthat/test_import_vdj.R
@@ -1,7 +1,7 @@
 
 test_that("Single import with full path", {
   res <- tiny_so %>%
-    import_vdj(vdj_dir = "inst/extdata/outs")
+    import_vdj(vdj_dir = system.file("extdata/outs", package = "djvdj"))
 
   expect_s4_class(res, "Seurat")                       # class
   expect_identical(colnames(res), colnames(tiny_so))   # cells in object
@@ -9,7 +9,7 @@ test_that("Single import with full path", {
 
 test_that("Single import without full path", {
   res <- tiny_so %>%
-    import_vdj(vdj_dir = "inst/extdata")
+    import_vdj(vdj_dir = system.file("extdata", package = "djvdj"))
 
   expect_s4_class(res, "Seurat")                       # class
   expect_identical(colnames(res), colnames(tiny_so))   # cells in object
