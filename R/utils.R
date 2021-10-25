8d03d77c10eb87fb5003c4c64efb70ff100f8373
rmsheridan <ryan.sheridan@ucdenver.edu>
2021-10-24 21:49:38 -0600

    test bug

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,39 @@
+#' Helper to test all combinations of provided arguments
+#'
+#' @param arg_lst Named list of arguments to test
+#' @param .fn Function to test
+#' @param desc Description to pass to test_that
+#' @param chk Function or expression to using for testing
+#' @param dryrun Do not run tests, just return table of arguments that will be
+#' tested
+#' @return Output from test_that
+test_all_args <- function(arg_lst, .fn, desc, chk, dryrun = FALSE) {
+
+  arg_lst <- expand.grid(arg_lst, stringsAsFactors = FALSE)
+
+  if (dryrun) {
+    return(arg_lst)
+  }
+
+  n <- 1
+
+  pwalk(arg_lst, ~ {
+    test_that(paste(desc, n), {
+
+      if (is.call(chk)) {
+        .res <- .fn(...)
+
+        return(eval(chk))
+      }
+
+      chk(.fn(...))
+    })
+
+    n <<- n + 1
+  })
+}
+
+
 #' Summarize values for chains
 #'
 #' Summarize values present for each column provided to the data_cols argument.
@@ -127,6 +163,7 @@ summarize_chains <- function(input, data_cols = c("umis", "reads"), fn, chain_co
   .to_tibble(input@colData, row_col)
 
 }
+
 
 #' Convert to tibble
 #'
