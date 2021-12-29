3b7c53b6f7a8457c0a96e50d064be192e84e5e27
rmsheridan <ryan.sheridan@ucdenver.edu>
2021-12-28 22:46:15 -0700

    summarize_vdj tests

--- tests/testthat/test-mutate-vdj.R
+++ tests/testthat/test-mutate-vdj.R
@@ -118,3 +118,120 @@ test_that("mutate_vdj bad command", {
       )
   )
 })
+
+
+
+
+
+# Check Seurat output
+test_that("summarize_vdj Seurat out", {
+  res <- vdj_so %>%
+    summarize_vdj(
+      vdj_cols = "umis",
+      col_names = "NEW_{.col}"
+    )
+
+  expect_s4_class(res, "Seurat")                     # class
+  expect_identical(colnames(res), colnames(vdj_so))  # cells in object
+  expect_identical(select(res@meta.data, -starts_with("NEW_")), vdj_so@meta.data)
+})
+
+# Check SCE input
+test_that("summarize_vdj SCE out", {
+  res <- vdj_sce %>%
+    summarize_vdj(
+      vdj_cols = "umis",
+      col_names = "NEW_{.col}"
+    )
+
+  expect_s4_class(res, "SingleCellExperiment")        # class
+  expect_identical(colnames(res), colnames(vdj_sce))  # cells in object
+  expect_identical(res@colData[, -ncol(res@colData)], vdj_sce@colData)
+})
+
+# Check data.frame output
+test_that("summarize_vdj df out", {
+  res <- vdj_so %>%
+    summarize_vdj(
+      vdj_cols = "umis",
+      col_names = "NEW_{.col}",
+      return_df = TRUE
+    )
+
+  expect_s3_class(res, "data.frame")                 # class
+  expect_identical(rownames(res), colnames(vdj_so))  # cells in object
+  expect_identical(select(res, -starts_with("NEW_")), vdj_so@meta.data)
+})
+
+# Check data.frame input
+test_that("summarize_vdj df in", {
+  res <- vdj_so@meta.data %>%
+    summarize_vdj(
+      vdj_cols = "umis",
+      col_names = "NEW_{.col}"
+    )
+
+  expect_s3_class(res, "data.frame")                 # class
+  expect_identical(rownames(res), colnames(vdj_so))  # cells in object
+  expect_identical(select(res, -starts_with("NEW_")), vdj_so@meta.data)
+})
+
+# Check calculations
+test_that("summarize_vdj calcs", {
+  res <- vdj_so@meta.data %>%
+    summarize_vdj(
+      vdj_cols = "umis",
+      col_names = "NEW_{.col}"
+    )
+
+  x <- vdj_so$umis %>%
+    strsplit(";") %>%
+    purrr::map_dbl(~ mean(as.numeric(.x))) %>%
+    unname()
+
+  expect_identical(x, res$NEW_umis)
+
+  res <- vdj_so@meta.data %>%
+    summarize_vdj(
+      vdj_cols  = "v_gene",
+      fn        = ~ ifelse(all(is.na(.x)), NA, paste0(.x, collapse = "_")),
+      col_names = "NEW_{.col}"
+    )
+
+  x <- vdj_so$v_gene %>%
+    strsplit(";") %>%
+    purrr::map_chr(~ ifelse(all(is.na(.x)), NA, paste0(.x, collapse = "_"))) %>%
+    unname()
+
+  expect_identical(x, res$NEW_v_gene)
+
+  # Check chain filtering
+  res <- vdj_so@meta.data %>%
+    summarize_vdj(
+      vdj_cols  = "umis",
+      col_names = "NEW_{.col}",
+      chain     = "IGK"
+    )
+
+  x <- strsplit(vdj_so$umis, ";") %>%
+    purrr::map(as.numeric)
+
+  y <- strsplit(vdj_so$chains, ";")
+
+  z <- purrr::map2_dbl(x, y, ~ ifelse(all(is.na(.x)), NA, mean(.x[.y == "IGK"]))) %>%
+    unname()
+
+  expect_equal(z, res$NEW_umis)
+  expect_identical(res$chains, unname(vdj_so$chains))
+})
+
+# Check bad command
+test_that("summarize_vdj bad command", {
+  expect_error(
+    res <- vdj_so %>%
+      summarize_vdj(
+        vdj_cols = "umis",
+        fn = ~ .x[chains == "IGK"]
+      )
+  )
+})
