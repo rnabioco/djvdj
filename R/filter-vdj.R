5051dfa9e177bdca90dd8816ade8d3174047f0db
rmsheridan <ryan.sheridan@ucdenver.edu>
2022-01-09 00:59:26 -0700

    revamp filter_vdj

--- R/filter-vdj.R
+++ R/filter-vdj.R
@@ -1,54 +1,52 @@
 #' Filter V(D)J data
 #'
+#' Remove V(D)J data for chains/cells that do not satisfy the provided
+#' condition
+#'
 #' @param input Single cell object or data.frame containing V(D)J data. If a
 #' data.frame is provided, cell barcodes should be stored as row names.
-#' @param filt Condition to use for filtering meta.data
-#' @param clonotype_col meta.data column containing clonotype IDs. This column
-#' is used to determine which cells have V(D)J data. If clonotype_col is NULL,
-#' filtering is performed regardless of whether V(D)J data is present for the
-#' cell.
-#' @param filter_cells Remove cells from the object that do not satisfy the
-#' filtering conditions. If FALSE, V(D)J data for the cell will be removed from
-#' the meta.data but no cells will be removed from the object.
+#' @param filt Condition to use for filtering V(D)J data. To allow for
+#' filtering of per-chain V(D)J data, the data for each cell is converted into
+#' a vector, e.g. 'IGH;IGK' is equivalent to c('IGH', 'IGK'). This allows R
+#' vector operations to be performed on the per-chain values. The filtering
+#' condition must return TRUE/FALSE for each chain or a single TRUE/FALSE for
+#' each cell.
 #' @param vdj_cols meta.data columns containing V(D)J data to use for
 #' filtering. If NULL, V(D)J data are automatically selected by identifying
 #' columns that have NAs in the same rows as clonotype_col.
+#' @param clonotype_col meta.data column containing clonotype IDs. This column
+#' is used to determine which columns contain V(D)J data.
 #' @param sep Separator used for storing per cell V(D)J data
 #' @return Object with filtered meta.data
 #'
 #' @examples
-#' # Only keep V(D)J data for cells with all productive and full length chains
-#' filter_vdj(
-#'   vdj_so,
-#'   all(productive) && all(full_length)
-#' )
+#' # Only include V(D)J data for productive chains
+#' filter_vdj(vdj_so, productive)
 #'
-#' # Only keep V(D)J data for cells with at least two IGK chains
-#' filter_vdj(
-#'   vdj_sce,
-#'   length(chains[chains == "IGK"]) > 2
-#' )
+#' # Only include V(D)J data for cells with paired chains
+#' filter_vdj(vdj_sce, paired)
 #'
-#' # Only keep V(D)J data for cells with at least one heavy and one light chain
+#' # Only include V(D)J data for cells with at least one heavy and one light chain
 #' filter_vdj(
 #'   vdj_so,
 #'   "IGH" %in% chains && any(c("IGK", "IGL") %in% chains)
 #' )
 #'
-#' # Remove cells from the object that do not meet filtering criteria
+#' # Only include V(D)J data for cells that have an IGH, IGK, and IGL chain
 #' filter_vdj(
-#'   vdj_so,
-#'   "IGH" %in% chains && any(c("IGK", "IGL") %in% chains),
-#'   filter_cells = TRUE
+#'   vdj_sce,
+#'   all(c("IGH", "IGK", "IGL") %in% chains)
 #' )
 #'
+#' # Remove chains that only have 1 UMI for support
+#' filter_vdj(vdj_sce, umis > 1)
+#'
+#' # Only include V(D)J data for heavy chains
+#' filter_vdj(vdj_so, chains == "IGH")
+#'
 #' @export
-filter_vdj <- function(input, filt, clonotype_col = "clonotype_id", filter_cells = FALSE,
-                       vdj_cols = NULL, sep = ";") {
-
-  if (!filter_cells && is.null(clonotype_col)) {
-    stop("clonotype_col must be provided when filter_cells is set to FALSE")
-  }
+filter_vdj <- function(input, filt, vdj_cols = NULL, clonotype_col = "clonotype_id",
+                       sep = ";") {
 
   # Format input data
   meta <- .get_meta(input)
@@ -71,11 +69,6 @@ filter_vdj <- function(input, filt, clonotype_col = "clonotype_id", filter_cells
 
   # Create list-cols for V(D)J columns that contain sep
   if (!purrr::is_empty(sep_cols)) {
-    sep_cols <- purrr::set_names(
-      x  = sep_cols,
-      nm = paste0(".", sep_cols)
-    )
-
     vdj <- .unnest_vdj(
       df_in    = vdj,
       sep      = sep,
@@ -84,88 +77,89 @@ filter_vdj <- function(input, filt, clonotype_col = "clonotype_id", filter_cells
     )
 
     vdj <- dplyr::rowwise(vdj)
-  }
+    vdj <- dplyr::mutate(vdj, .KEEP = list({{filt}}))
+    vdj <- dplyr::ungroup(vdj)
 
-  # Store results for filtering
-  vdj <- dplyr::mutate(vdj, .KEEP = {{filt}})
-  vdj <- dplyr::ungroup(vdj)
+    length_one <- map_lgl(vdj$.KEEP, ~ length(.x) == 1)
+    length_one <- all(length_one)
 
-  # Remove V(D)J data from meta.data (without filtering cells)
-  if (!filter_cells) {
-    vdj <- dplyr::mutate(
-      vdj,
-      across(
-        all_of(c(vdj_cols, names(sep_cols))),
-        ~ ifelse(.KEEP, .x, NA)
-      )
-    )
-
-  # Filter cells from object
-  # when clonotype_col != NULL only filter cells with V(D)J data
   } else {
-    if (!is.null(clonotype_col)) {
-      vdj <- dplyr::mutate(
-        vdj,
-        .KEEP = dplyr::if_else(
-          is.na(!!sym(clonotype_col)),
-          TRUE,
-          .data$.KEEP
-        )
-      )
-    }
+    length_one <- TRUE
 
-    vdj <- dplyr::filter(vdj, .data$.KEEP)
+    vdj <- dplyr::mutate(vdj, .KEEP = {{filt}})
   }
 
-  # Remove columns created for filtering
-  if (!purrr::is_empty(sep_cols)) {
-    sep_cols <- purrr::set_names(
-      x  = names(sep_cols),
-      nm = unname(sep_cols)
-    )
-
-    vdj <- dplyr::select(vdj, -all_of(names(sep_cols)))
-    vdj <- dplyr::rename(vdj, !!!syms(sep_cols))
-  }
+  keep_rows <- vdj$.KEEP
 
   vdj <- dplyr::select(vdj, -.data$.KEEP)
 
+  # If vectors in keep_rows > length 1, filter chains
+  if (!length_one) {
+    for (clmn in sep_cols) {
+      x <- vdj[[clmn]]
+
+      x <- purrr::map2_chr(x, keep_rows, ~ {
+        if (!all(is.na(.x))) {
+          if (length(.x) != length(.y)) {
+            stop(
+              "Filtering condition must return TRUE/FALSE for each chain, ",
+              "or a single TRUE/FALSE for each cell."
+            )
+          }
+        }
+
+        ifelse(
+          all(is.na(.x)) || purrr::is_empty(.x[.y]),
+          as.character(NA),
+          paste0(.x[.y], collapse = sep)
+        )
+      })
+
+      vdj <- dplyr::mutate(vdj, !!sym(clmn) := x)
+    }
+
+    # Set NAs for vdj_cols not in sep_cols
+    other_cols <- vdj_cols[!vdj_cols %in% sep_cols]
+
+    vdj <- dplyr::mutate(
+      vdj,
+      across(all_of(other_cols), ~ {
+        ifelse(
+          if_all(all_of(sep_cols), is.na),
+          NA,
+          .x
+        )
+      })
+    )
+
+  # If vectors in keep_rows == length 1, filter V(D)J data per cell
+  } else {
+    keep_rows <- unlist(keep_rows)
+
+    for (clmn in vdj_cols) {
+      x <- vdj[[clmn]]
+
+      # Collapse sep_cols
+      if (!purrr::is_empty(sep_cols) && clmn %in% sep_cols) {
+        x <- purrr::map2_chr(x, keep_rows, ~ {
+          ifelse(
+            all(is.na(.x)) | !.y,
+            as.character(NA),
+            paste0(.x, collapse = sep)
+          )
+        })
+
+      } else {
+        x <- ifelse(keep_rows, x, NA)
+      }
+
+      vdj <- dplyr::mutate(vdj, !!sym(clmn) := x)
+    }
+  }
+
   # Format results
-  in_cols <- colnames(meta)
-
-  vdj <- dplyr::relocate(vdj, all_of(in_cols), .after = .data$.cell_id)
-
-  cells <- vdj$.cell_id
-  res   <- .subset_cells(input, cells)
-  res   <- .add_meta(res, meta = vdj)
+  res <- .add_meta(input, vdj)
 
   res
 }
-
-#' Subset object so it only contains given cells
-#'
-#' @param input Single cell object or data.frame. If a data.frame is provided, the
-#' cell barcodes should be stored as row names.
-#' @param cells Vector with cell barcodes to use for subsetting
-#' @return Subsetted object
-#' @noRd
-.subset_cells <- function(input, cells) {
-
-  UseMethod(".subset_cells", input)
-}
-
-.subset_cells.default <- function(input, cells) {
-
-  res <- input[, cells]
-
-  res
-}
-
-.subset_cells.data.frame <- function(input, cells) {
-
-  res <- input[rownames(input) %in% cells, ]
-
-  res
-}
-
 
