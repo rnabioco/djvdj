210c7bad55896d053a1be26af4252f4dd9ad180c
rmsheridan <ryan.sheridan@ucdenver.edu>
2022-01-10 22:57:37 -0700

    more calc_vdj_usage tests

--- tests/testthat/test-calc-vdj-usage.R
+++ tests/testthat/test-calc-vdj-usage.R
@@ -4,7 +4,7 @@ df_1 <- vdj_so@meta.data
 df_2 <- vdj_so@meta.data %>%
   as_tibble(rownames = ".cell_id")
 
-# Check all calc_usage arguments
+# Check all calc_vdj_usage arguments
 arg_lst <- list(
   input       = list(vdj_so, vdj_sce, df_1, df_2),
   gene_cols   = list("v_gene", "d_gene", "j_gene", "c_gene", c("v_gene", "j_gene")),
@@ -19,3 +19,124 @@ test_all_args(
   desc    = "calc_usage args",
   chk     = expr(expect_s3_class(.res, "tbl"))
 )
+
+# Check calc_vdj_usage calculations
+.check_vdj_usage <- function(input, genes, chain = NULL) {
+
+  # calc_vdj_usage results
+  res <- calc_vdj_usage(
+    input,
+    gene_cols = genes,
+    chain = chain
+  ) %>%
+    rowwise() %>%
+    mutate(nm = stringr::str_c(!!!syms(genes))) %>%
+    arrange(desc(freq), nm)
+
+  freq <- purrr::set_names(res$freq, res$nm)
+  pct  <- purrr::set_names(res$pct, res$nm)
+
+  # data for manual calc
+  dat <- vdj_so@meta.data %>%
+    filter(!is.na(clonotype_id))
+
+  gns <- genes
+
+  if (!is.null(chain)) {
+    gns <- c("chains", genes)
+  }
+
+  x <- gns %>%
+    purrr::set_names() %>%
+    map(~ {
+      dat[[.x]] %>%
+        map(stringr::str_split, ";") %>%
+        purrr::flatten()
+    })
+
+  # Get total n cells
+  n_cells <- map_int(x, length) %>%
+    unique()
+
+  stopifnot(length(n_cells) == 1)
+
+  # Filter chains
+  if (!is.null(chain)) {
+    chns <- x$chains
+
+    chns <- chns %>%
+      map(~ .x %in% chain)
+
+    x <- x[names(x) != "chains"]
+
+    x <- x %>%
+      map(~ purrr::map2(.x, chns, ~ .x[.y])) %>%
+      map(~ map(.x, ~ {
+        if (purrr::is_empty(.x)) .x <- "None"
+        .x
+      }))
+  }
+
+  # Calc frequency
+  x <- x %>%
+    reduce(~ {
+      stopifnot(length(.x) == length(.y))
+      purrr::map2(.x, .y, ~ stringr::str_c(.x, .y))
+    }) %>%
+    map(unique) %>%
+    reduce(c) %>%
+    table() %>%
+    sort(decreasing = TRUE)
+
+  # Check results
+  x <- purrr::set_names(
+    as.integer(x),
+    names(x)
+  )
+
+  expect_identical(x, freq)
+  expect_identical((x / n_cells) * 100, pct)
+}
+
+test_that("calc_vdj_usage check calcs", {
+
+  vdj_genes <- c(
+    "v_gene", "d_gene",
+    "j_gene", "c_gene"
+  )
+
+  vdj_genes <- seq_along(vdj_genes) %>%
+    map(combn, x = vdj_genes, simplify = FALSE) %>%
+    purrr::flatten()
+
+  ins <- list(vdj_so, vdj_sce, vdj_so@meta.data)
+
+  purrr::walk(ins, ~ {
+    vdj_genes %>%
+      purrr::walk(.check_vdj_usage, input = .x)
+  })
+
+  purrr::walk(ins, ~ {
+    vdj_genes %>%
+      purrr::walk(.check_vdj_usage, input = .x, chain = "IGH")
+  })
+
+  purrr::walk(ins, ~ {
+    vdj_genes %>%
+      purrr::walk(.check_vdj_usage, input = .x, chain = c("IGH", "IGK"))
+  })
+})
+
+# Bad vdj gene
+test_that("calc_vdj_usage bad gene", {
+
+  ins <- list(vdj_so, vdj_sce, vdj_so@meta.data)
+
+  purrr::walk(ins, ~ {
+    expect_error(
+      calc_vdj_usage(.x, "BAD"),
+      "Column .+ doesn't exist"
+    )
+  })
+})
+
