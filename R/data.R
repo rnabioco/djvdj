edbfc948ec43d1db637cda90e81ca2a4906d5faf
rmsheridan@cassini <ryan.sheridan@ucdenver.edu>
2022-01-12 22:46:14 -0700

    added data to function ref

--- R/data.R
+++ R/data.R
@@ -1,23 +1,23 @@
 #' Seurat object for tests
 #'
-#' A tiny Seurat object for tests
-#' @format A Seurat object with 200 features, 200 cells, and 1 assay
+#' A tiny Seurat object
+#' @format A Seurat object with 200 features, 204 cells, and 1 assay
 "tiny_so"
 
 #' Seurat object with V(D)J data for tests
 #'
-#' A tiny Seurat object with V(D)J data for tests
-#' @format A Seurat object with 200 features, 200 cells, and 1 assay
+#' A tiny Seurat object with V(D)J data
+#' @format A Seurat object with 200 features, 204 cells, and 1 assay
 "vdj_so"
 
 #' SingleCellExperiment object for tests
 #'
-#' A tiny SingleCellExperiment object for tests
-#' @format A SingleCellExperiment object with 200 features, 200 cells, and 1 assay
+#' A tiny SingleCellExperiment object
+#' @format A SingleCellExperiment object with 200 features, 204 cells, and 1 assay
 "tiny_sce"
 
 #' SingleCellExperiment object with V(D)J data for tests
 #'
-#' A tiny SingleCellExperiment object with V(D)J data for tests
-#' @format A SingleCellExperiment object with 200 features, 200 cells, and 1 assay
+#' A tiny SingleCellExperiment object with V(D)J data
+#' @format A SingleCellExperiment object with 200 features, 204 cells, and 1 assay
 "vdj_sce"
