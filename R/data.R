e85e4b4c1d46afe15b50329e5853d8e37a550f4e
rmsheridan@cassini <ryan.sheridan@ucdenver.edu>
2022-01-12 23:27:32 -0700

    vignette errors

--- R/data.R
+++ R/data.R
@@ -1,22 +1,22 @@
-#' Seurat object for tests
+#' A tiny Seurat object
 #'
 #' A tiny Seurat object
 #' @format A Seurat object with 200 features, 204 cells, and 1 assay
 "tiny_so"
 
-#' Seurat object with V(D)J data for tests
+#' A tiny Seurat object with V(D)J data
 #'
 #' A tiny Seurat object with V(D)J data
 #' @format A Seurat object with 200 features, 204 cells, and 1 assay
 "vdj_so"
 
-#' SingleCellExperiment object for tests
+#' A tiny SingleCellExperiment object
 #'
 #' A tiny SingleCellExperiment object
 #' @format A SingleCellExperiment object with 200 features, 204 cells, and 1 assay
 "tiny_sce"
 
-#' SingleCellExperiment object with V(D)J data for tests
+#' A tiny SingleCellExperiment object with V(D)J data
 #'
 #' A tiny SingleCellExperiment object with V(D)J data
 #' @format A SingleCellExperiment object with 200 features, 204 cells, and 1 assay
