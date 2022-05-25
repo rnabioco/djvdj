e6db8ef03807eb940dfbed4e64b28430307f80af
rmsheridan@cassini <ryan.sheridan@ucdenver.edu>
2022-05-25 15:22:03 -0600

    mutate_vdj update docs

--- R/mutate-vdj.R
+++ R/mutate-vdj.R
@@ -118,7 +118,8 @@ fetch_vdj <- function(input, vdj_cols = NULL, clonotype_col = NULL, filter_cells
 #' same rows as clonotype_col.
 #' @param return_df Return results as a data.frame. If FALSE, results will be
 #' added to the input object.
-#' @param sep Separator used for storing per cell V(D)J data
+#' @param sep Separator used for storing per cell V(D)J data. If NULL, columns
+#' containing V(D)J data will not be converted to vectors for filtering.
 #' @return Object with modified meta.data
 #'
 #' @examples
@@ -185,7 +186,7 @@ mutate_vdj <- function(input, ..., clonotype_col = "clonotype_id", vdj_cols = NU
   sep_cols <- col_list$sep
 
   # Allow sep to be NULL so user can skip .unnest_vdj
-  if (purrr::is_empty(sep_cols) && !is.null(sep)) {
+  if (!is.null(sep) && purrr::is_empty(sep_cols)) {
     warning("The separator '", sep, "' is not present in the data")
   }
 
